af0afb473d3253f19b5e93a3007c395a771d8fd0
Marcus Aldrin <71566523+m-kj-aldrin@users.noreply.github.com>
2023-10-12 16:00:49 +0200

    simpler range

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import "./inputs/index.js";
 
 const inpRange0 = document.createElement("input-range");
+inpRange0.width = 64
+// inpRange0.style.width = "64px";
 inpRange0.minmax = { min: 0, max: 1 };
-inpRange0.steps = 1024;
+inpRange0.steps = 4096;
 inpRange0.value = 0.5;
+
+const inpRange1 = document.createElement("input-range");
+inpRange1.width = 512
+// inpRange1.style.width = "512px";
+inpRange1.minmax = { min: 0, max: 1 };
+inpRange1.steps = 128;
+inpRange1.value = 0.5;
 
 const inpSelect0 = document.createElement("input-select");
 inpSelect0.list = ["sine", "tri", "ramp up", "ramp down"];
@@ -18,8 +27,15 @@ inpPicker0.setPickerType({
 
 const inpNumber0 = document.createElement("input-number");
 
-document.body.append(inpRange0, inpSelect0, inpSwitch0, inpPicker0, inpNumber0);
+document.body.append(
+  inpRange0,
+  inpRange1,
+  inpSelect0,
+  inpSwitch0,
+  inpPicker0,
+  inpNumber0
+);
 
 document.body.addEventListener("change", (e) => {
-  // console.log(e.target.value);
+  console.log(e.target.value);
 });
--- inputs/range.js
+++ inputs/range.js
@@ -5,8 +5,10 @@ const rangeTemplate = `
 <style>
     :host{
         cursor: pointer;
-        width: 144px;
+        width: max-content;
         position: relative;
+
+        width: 64px;
     }
 
     * {
@@ -37,8 +39,8 @@ const rangeTemplate = `
         transition: r 200ms ease;
     }
 
-    svg:active circle{
-        r: 5px;
+    :host(:active) circle,:host(:focus) circle{
+        r: 4px;
         fill: none;
         stroke: currentColor;
     }
@@ -71,33 +73,24 @@ const rangeTemplate = `
     }
 
     svg {
-        outline: 1px red solid;
-        --w: 256px;
-        --h: 16px;
-        width: 100%;
-        height: var(--h);
+        outline: 1px currentColor solid;
         padding-inline: 8px;
-
-    }
-
-    path {
+        width: 100%;
     }
 
     </style>
-    <svg tabindex="0">
-        <g id="track" transform="translate(0 8)">
-        <rect width="100%" height="2" y="-1" rx="4"></rect>
-        <circle cx="0" cy="0" r="4" fill="currentColor" />
-            <text dy="-8" id="stepping">1</text>
-        </g>
-    </svg>
-    <input-number></input-number>
-    `;
+    <svg tabindex="0" height="16">
+    <g id="track" transform="translate(0 8)">
+        <rect width="100%" height="2" y="-1"></rect>
+        <circle cx="0" cy="0" r="3" fill="currentColor" />
+        <text dy="-8" id="stepping">1</text>
+    </g>
+</svg>
+<input-number></input-number>
+`;
 
-// <line x1="0" x2="128" stroke="currentColor"></line>
-// <path d="M0,0 h128" stroke="currentColor" line-width="1" stroke-linecap="round" />
 function clamp(x, min, max) {
-    return Math.min(Math.max(x, min), max);
+  return Math.min(Math.max(x, min), max);
 }
 
 /**
@@ -105,121 +98,121 @@ function clamp(x, min, max) {
  * @param {number} q
  */
 function quantize(x, q) {
-    let v = Math.floor(x * q) / q;
-    return v;
+  let v = Math.floor(x * q) / q;
+  return v;
 }
 
 // TODO - make x size of slider dynamic
 export class InputRange extends Base {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        /**@private */
-        this._value = null;
+    /**@private */
+    this._value = null;
 
-        /**@private */
-        this._minmax = { min: null, max: null };
+    /**@private */
+    this._minmax = { min: null, max: null };
 
-        /**@private */
-        this._step = 1;
+    /**@private */
+    this._step = 1;
 
-        this._f = 1;
+    this._f = 1;
+    /**@type {DOMRect} */
+    this._box = null;
 
-        this.shadowRoot.innerHTML += rangeTemplate;
-        this.svg = this.shadowRoot.querySelector("svg");
-        /**@type {DOMRect} */
-        this._box = null;
-        this._w = 0;
+    this._width = 0;
 
-        this.svg.onpointerdown = (e) => {
-            this.svg.setPointerCapture(e.pointerId);
+    this.shadowRoot.innerHTML += rangeTemplate;
+    this.svg = this.shadowRoot.querySelector("svg");
 
-            let box = this.svg.getBoundingClientRect();
-            this._box = box;
+    this.svg.onpointerdown = (e) => {
+      this.svg.setPointerCapture(e.pointerId);
 
-            let w = box.width - 16;
-            this._w = w;
-            let s = this._step < w ? 1 : w / this._step;
-            let x = e.clientX - 8 - 8;
-            x = clamp(x, 0, w);
-            this.value = x / w;
+      let box = this.svg.getBoundingClientRect();
+      console.log(box.width, this.width);
+      this._box = box;
 
-            this.svg.onpointermove = (ee) => {
-                let movement = ee.movementX * s;
-                x = x + movement;
-                x = clamp(x, 0, w);
-                this.value = x / w;
-                // if (ee.ctrlKey) {
-                //     !startY && (startY = ee.clientY);
-                //     let y = ee.clientY - startY;
-                //     y *= -1;
-                //     let yQ = Math.floor(y / 10) * 4;
-                //     yQ = clamp(yQ, 1, 32);
-                //     stepF = yQ;
-                //     this.svg.toggleAttribute("moving", true);
-                //     this.svg.querySelector("#stepping").textContent =
-                //         stepF.toString();
-                // }
-                // let x = cur + ee.movementX * this._f * stepF;
-                // x = clamp(x, 0, 128);
-                // this.value = x / 128;
-                // cur = x;
-            };
-        };
+      //   let w = box.width - 16;
+      let w = this.width;
+      this._width = w;
+      let x = e.clientX - box.x - 8;
+      x = clamp(x, 0, w);
 
-        let prevVal = null;
+      this.value = x / w;
 
-        this.svg.onpointerup = (e) => {
-            if (prevVal != this.value) {
-                prevVal = this.value;
-                this.dispatchEvent(new Event("change", { bubbles: true }));
-            }
-            this.svg.removeAttribute("moving");
-            this.svg.releasePointerCapture(e.pointerId);
-            this.svg.onpointermove = null;
-        };
+      let ratio = this._step > w ? w / this._step : 1;
 
-        this.shadowRoot.addEventListener(
-            "change",
-            /**@param {InputEvent & {target: InputNumber}} e */
-            (e) => {
-                this.value = clamp(e.target.value / this._step, 0, 1);
-            }
-        );
+      this.svg.onpointermove = (ee) => {
+        x = x + ee.movementX * ratio;
+        x = clamp(x, 0, w);
+        this.value = x / w;
+      };
+    };
+
+    let prevVal = null;
+
+    this.svg.onpointerup = (e) => {
+      if (prevVal != this.value) {
+        prevVal = this.value;
+        this.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+      this.svg.removeAttribute("moving");
+      this.svg.releasePointerCapture(e.pointerId);
+      this.svg.onpointermove = null;
+    };
+
+    this.shadowRoot.addEventListener(
+      "change",
+      /**@param {InputEvent & {target: InputNumber}} e */
+      (e) => {
+        this.value = clamp(e.target.value / this._step, 0, 1);
+      }
+    );
+  }
+
+  set width(v) {
+    this._width = v + 16;
+    this.style.width = `${v}px`;
+  }
+
+  get width() {
+    // const box = this.svg.getBoundingClientRect();
+    // return box.width - 16;
+    return this._width;
+  }
+
+  /**@param {{min:number,max:number}} o */
+  set minmax({ min, max }) {
+    this._minmax = { min, max };
+  }
+
+  get minmax() {
+    return this._minmax;
+  }
+
+  /**@param {number} n */
+  set steps(n) {
+    this._step = n;
+  }
+
+  /**@param {number} v */
+  set value(v) {
+    const f = v;
+    if (this._value != f) {
+      this._value = f;
+      let x = f * this.width;
+      //   console.log(x);
+      this.svg.querySelector("circle").setAttribute("cx", x.toString());
+      this.shadowRoot.querySelector(
+        "input-number"
+      ).style.left = `${x.toString()}px`;
+      this.shadowRoot.querySelector("input-number").value = Math.round(
+        f * this._step
+      );
     }
+  }
 
-    /**@param {{min:number,max:number}} o */
-    set minmax({ min, max }) {
-        this._minmax = { min, max };
-    }
-
-    get minmax() {
-        return this._minmax;
-    }
-
-    /**@param {number} n */
-    set steps(n) {
-        // const box = this.svg.getBoundingClientRect();
-        // const w = box.width - 16;
-        // this._f = w / n;
-        this._step = n;
-    }
-
-    /**@param {number} v */
-    set value(v) {
-        const f = v;
-        console.log(v);
-        if (this._value != f) {
-            this._value = f;
-            this.svg.querySelector("circle").setAttribute("cx", this._w * f);
-            // const inpNum = this.shadowRoot.querySelector("input-number");
-            // inpNum.style.left = `${128 * f}px`;
-            // inpNum.value = Math.round(this._value * this._step);
-            // this.dispatchEvent(new Event("input", { bubbles: true }));
-        }
-    }
-
-    get value() {
-        return this._value;
-    }
+  get value() {
+    return this._value;
+  }
 }
